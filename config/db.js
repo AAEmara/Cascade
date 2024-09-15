bfc96bfbce50060e26fbf37caddadbaf00015c47
AAEmara <abdelrahmanemara18@gmail.com>
2024-09-15 12:00:09 +0300

    CSADE: Fixed a typo in the routes.
    Fixed the 'api/auth' route, where we added
    the missing '/' and also used a better syntax
    to define and export the `connectDB` function
    inside the 'db.js' file.

--- config/db.js
+++ config/db.js
@@ -1,23 +1,16 @@
-#!/usr/bin/node
-
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const connectDB = async () => {
+export default async function connectDB() {
   const dbURI = process.env.NODE_ENV === 'production' ?
     process.env.MONGO_URI_PROD : process.env.MONGO_URI_DEV;
 
   try {
-    await mongoose.connect(dbURI, {
+    const conn = await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log('MongoDB connection failed:', error.message);
   }
 };
-
-export default connectDB;
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 
-dotenv.config();
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' :
+  '.env.development';
+dotenv.config({ path: envFile });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,7 +16,7 @@ connectDB();
 
 app.use(express.json());
 
-app.use('api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
